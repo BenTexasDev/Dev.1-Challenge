b3a77c3ccd7f366eded7ae7e816d89ed9499a02a
Ben Barela <benitobarela@yahoo.com>
2019-05-24 20:36:54 -0500

    Comments

--- D1P_Challenge.py
+++ D1P_Challenge.py
@@ -5,31 +5,39 @@ import re
 # Need to add add output and EOF. Some error checking as well.
 
 fname = input('Enter the file name: ')
-file = open('output.txt', 'w')
 if len(fname) < 1 : fname = 'input.txt'
+
+# try to open file
 try:
 	fhand = open(fname)
+# If file does not exist or can't be opened throw an error	
 except:
-	print ('File cannot be opened:',fname)
+	print ('Error:File cannot be found:', fname)
 	exit()
+# Create output file after file has been opened with write flag.
+outputfile = open('output.txt', 'w')
 
-
+# Create dictionary to store each word, line by line.
 di = dict()
-
+# Iterate through each line in the file.
 for line in fhand:
-
+	# We want to strip newline.
     line = line.rstrip('\n')
+	# Returns copy of the string in which all case-based characters have been lowercased.
     line = line.lower()
-    line = re.sub('[,.]','', line)
+	# Sub special characters in the file with nothing. (Delete the file)
+    line = re.sub('[,.!@#]','', line)
+	# Split line into list of strings
     words = line.split()
-
+	# Iterate through the list and count occurences of each word. di[w] = occurences,
+	# Take those occurences and store in the dictionary.
     for w in words:
     	if w not in di:
     		di[w] = 1
     	else:
     		di[w] += 1
-
+# Sorted iterates over the dictionary keys, using the number of word occurences as a sort key.
 for w in sorted(di, key=di.get, reverse = True):
-        file.write("{:8}{:2}{:5}{}".format(w,"|",di[w]*"=","("+str(di[w])+")""\n"))
+        outputfile.write("{:8}{:2}{:5}{}".format(w,"|",di[w]*"=","("+str(di[w])+")""\n"))
 
-file.close()
+outputfile.close()
