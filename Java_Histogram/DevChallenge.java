8872aed932461336e38eea5be7c996ad381a535c
Ben Barela <benitobarela@yahoo.com>
2019-05-25 14:37:06 -0500

    Equal sign occurences

--- Java_Histogram/DevChallenge.java
+++ Java_Histogram/DevChallenge.java
@@ -1,43 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-public class DevChallenge{
+public class DevChallenge {
 
-    public static void main(String args[]) throws Exception
-    {
+    public static void main(String args[]) throws Exception {
         BufferedReader infile = new BufferedReader(new FileReader("Java_Histogram/input.txt"));
         String line;
-        HashMap<String,Integer> histogram = new HashMap<String,Integer>();
-        while (( line = infile.readLine()) != null )
-        {   
-            
-            line = line.replace(".","").replace(",","").toLowerCase();
-            String [] words = line.split(" ");
-            //System.out.println(line);
+        HashMap<String, Integer> histogram = new HashMap<String, Integer>();
+        while ((line = infile.readLine()) != null) {
 
-    
-            for (String word : words)
-            {
+            line = line.replace(".", "").replace(",", "").toLowerCase();
+            String[] words = line.split(" ");
+            // System.out.println(line);
+
+            for (String word : words) {
                 Integer f = histogram.get(word);
                 if (f == null) {
-                    histogram.put(word,1);
+                    histogram.put(word, 1);
                 } else {
-                    histogram.put(word,f+1);
-                    //System.out.println(histogram);
+                    histogram.put(word, f + 1);
+                    // System.out.println(histogram);
                 }
-                
-            }   
+
+            }
         }
         infile.close();
-        printHistogram( histogram );
-    }
-    private static void printHistogram( HashMap<String,Integer> hm )
-    {
-        for(String h : hm.keySet()){
-            int value = hm.get(h);
-            System.out.println(h + "|" + "("+ value +")");
+        printHistogram(histogram);
     }
 
+    private static void printHistogram(HashMap<String, Integer> hm) {
+        for (String h : hm.keySet()) {
+
+            int value = hm.get(h);
+            System.out.printf(h + "|");
+
+            for (int i = 0; i < value; i++) {
+                System.out.printf("=");
+            }
+            System.out.printf("(" + value + ")\n");
         }
-        
+
+    }
 }
