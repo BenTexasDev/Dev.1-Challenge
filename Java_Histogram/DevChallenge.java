2282473f1bd32b9159f6fe620260d598de27da28
Ben Barela <benbarela.tx@gmail.com>
2019-05-25 20:25:55 -0500

    format code.
    Used prettier extension to format code a bit better.

--- Java_Histogram/DevChallenge.java
+++ Java_Histogram/DevChallenge.java
@@ -3,14 +3,14 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-public class DevChallenge 
-{
+public class DevChallenge {
 
     private static boolean DESC = false;
+
     public static void main(String args[]) throws Exception {
         BufferedReader infile = new BufferedReader(new FileReader("Java_Histogram/input.txt"));
         String line;
-        Map<String, Integer> histogram = new HashMap<String, Integer> ();
+        Map<String, Integer> histogram = new HashMap<String, Integer>();
         while ((line = infile.readLine()) != null) {
 
             line = line.replace(".", "").replace(",", "").toLowerCase();
@@ -23,44 +23,40 @@ public class DevChallenge
                     histogram.put(word, 1);
                 } else {
                     histogram.put(word, f + 1);
-                    // System.out.println(histogram);
                 }
 
             }
         }
         infile.close();
 
-        Map<String,Integer> sortedMapDesc = sortByValue(histogram, DESC);
+        Map<String, Integer> sortedMapDesc = sortByValue(histogram, DESC);
         printHistogram(sortedMapDesc);
     }
 
-    private static Map <String,Integer> sortByValue( Map<String, Integer> histogram, final boolean order) {
+    private static Map<String, Integer> sortByValue(Map<String, Integer> histogram, final boolean order) {
         List<Entry<String, Integer>> list = new LinkedList<>(histogram.entrySet());
 
         // Sorting the list based on values
-        list.sort((o1, o2) -> order ? o1.getValue().compareTo(o2.getValue()) == 0
-                ? o1.getKey().compareTo(o2.getKey())
-                : o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
-                ? o2.getKey().compareTo(o1.getKey())
-                : o2.getValue().compareTo(o1.getValue()));
+        list.sort((o1, o2) -> order
+                ? o1.getValue().compareTo(o2.getValue()) == 0 ? o1.getKey().compareTo(o2.getKey())
+                        : o1.getValue().compareTo(o2.getValue())
+                : o2.getValue().compareTo(o1.getValue()) == 0 ? o2.getKey().compareTo(o1.getKey())
+                        : o2.getValue().compareTo(o1.getValue()));
         return list.stream().collect(Collectors.toMap(Entry::getKey, Entry::getValue, (a, b) -> b, LinkedHashMap::new));
 
     }
 
-    private static void printHistogram(Map<String, Integer> map)
-    {
-        for (String m : map.keySet()){
+    private static void printHistogram(Map<String, Integer> map) {
+        for (String m : map.keySet()) {
 
             int value = map.get(m);
             System.out.printf(m + "|");
-            
-            for(int i = 0; i < value; i++){
+
+            for (int i = 0; i < value; i++) {
                 System.out.printf("=");
             }
 
-            System.out.printf("("+value+")\n");
+            System.out.printf("(" + value + ")\n");
         }
     }
 }
-
-
